529f6c2d1c193e0bfb900971436ff86b907d3d7f
Gregor Swatek <gregor.swatek@gmail.com>
2013-07-14 00:25:32 +0200

    try something out

--- js/application.js
+++ js/application.js
@@ -1,28 +1,35 @@
 $(document).ready(function() {
-	$('.lumia-top-navbar').onePageNav({
-    currentClass: 'active',
-    changeHash: true,
-    scrollSpeed: 750,
-    scrollOffset: 0,
-    scrollThreshold: 0.5,
-    filter: '',
-    easing: 'swing',
-
-    begin: function() {
-      //I get fired when the animation is starting
-      //Hack so you can click other menu items after the initial click
-      $('body').append('<div id="device-dummy" style="height: 1px;"></div>');
-    },
-    
-    end: function() {
-      //I get fired when the animation is ending
-      $('#device-dummy').remove();
-    },
-    
-    scrollChange: function($currentListItem) {
-      //I get fired when you enter a section and I pass the list item of the section
-    }
-  });
+  $('.lumia-top-navbar').onePageNav({
+      currentClass: 'active',
+      changeHash: true,
+      scrollSpeed: 700,
+      scrollOffset: 0,
+      scrollThreshold: 0.5,
+      filter: '',
+      easing: 'swing',
+  
+      begin: function() {
+        //I get fired when the animation is starting
+        //Hack so you can click other menu items after the initial click
+        $('body').append('<div id="device-dummy" style="height: 1px;"></div>');
+      },
+      
+      end: function() {
+        //I get fired when the animation is ending
+        $('#device-dummy').remove();
+      },
+      
+      scrollChange: function($currentListItem) {
+        //I get fired when you enter a section and I pass the list item of the section
+      }
+    });
+  
+  // $('.lumia-top-navbar > a').click(function(e) {
+  //   e.preventDefault();
+  //   $('html, body').animate({
+  //     scrollTop: $("#lumia-device-infos").offset().top
+  //   }, 2000);    
+  // });
   
   if ( Modernizr.csstransforms ) {
     var elem = document.getElementById('slider');
@@ -37,17 +44,18 @@ $(document).ready(function() {
     });
   }
   
-	$('.lumia-device-info-category-title').click(function() {
+	$('.lumia-device-info-category-title').click(function(e) {
 
 		$('.lumia-device-info-category-title').removeClass('active');	  
 	 	$('.lumia-device-info-category-content').slideUp('normal');
+
  
 		if($(this).next().is(':hidden')) {		
 			$(this).addClass('active');
-			$(this).next().slideDown('normal');
-		 } 
+			$(this).next().slideDown('normal');      
+		} 
 	  
-	 });
+  });
 
-   $('.lumia-device-info-category-content').hide();
+  $('.lumia-device-info-category-content').hide();
 });
