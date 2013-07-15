4848f30d529758957a3d8cdcb2af60fa6232536c
Gregor Swatek <gregor.swatek@gmail.com>
2013-07-15 18:10:00 +0200

    added slider buttons, set new position on resize window

--- js/application.js
+++ js/application.js
@@ -1,22 +1,46 @@
+
 $(document).ready(function() {  
+
+  $('.slider-btn').hide();
+
+  $(".swipe-wrap > div > img").load(function() {
+    calculation();
+    $('.slider-btn').fadeIn();    
+  });
+
+  $(window).resize(calculation);
+  
+  function calculation() {
+    var elem = $(".swipe-wrap > div > img");
+    var top_offset = elem.offset().top;
+    var slider_btn_height = $('.slider-btn').height() / 2;
+    var pos = (elem.height() / 2) + top_offset - slider_btn_height;
+
+    $('.slider-btn').css('top', pos + 'px');
+  }
+  
   $('.lumia-top-navbar > a').click(function(e) {
     e.preventDefault();
     $('html, body').animate({
     	scrollTop: $("#lumia-device-infos").offset().top
     }, 2000);    
   });
+
   
+
   if ( Modernizr.csstransforms ) {
     var elem = document.getElementById('slider');
     window.mySwipe = Swipe(elem, {
       // startSlide: 4,
       // auto: 3000,
-      // continuous: true,
+      continuous: true
       // disableScroll: true,
       // stopPropagation: true,
       // callback: function(index, element) {},
       // transitionEnd: function(index, element) {}
     });
+    $('.slider-btn.next').click(function() { mySwipe.next(); });
+    $('.slider-btn.prev').click(function() { mySwipe.prev(); });
   }
   
 	$('.lumia-device-info-category-title').click(function(e) {
@@ -33,4 +57,6 @@ $(document).ready(function() {
   });
 
   $('.lumia-device-info-category-content').hide();
+
+
 });
